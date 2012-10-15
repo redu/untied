4db8350ffcc9fc95cf50758c37c17c5c52ae8ab7
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2012-10-15 07:51:01 -0300

    Adding basic MQ worker (without consumer)

---
+++ lib/untied/worker.rb
@@ -0,0 +1,22 @@
+module Untied
+  class Worker
+    def initialize(opts)
+      @channel = opts[:channel]
+      @queue_name = opts[:queue_name] || ""
+      # @consumer = opts[:consumer] || Consumer.new
+      @exchange = opts[:exchange]
+
+      Untied.config.logger.info "Worker initialized and listening"
+    end
+
+    def start
+      @channel.queue(@queue_name, :exclusive => true) do |queue|
+        queue.bind(@exchange, :routing_key => "untied.#").subscribe do |h,p|
+          Untied.config.logger.info "Message #{p} with headers #{h} arrived"
+          # p = JSON.parse(p, :symbolize_keys => true)
+          # @consumer.call(h,p)
+        end
+      end
+    end
+  end
+end
