c128b96c56b5ae5f8e4d37c2054a1115ed00586e
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2012-10-17 08:31:49 -0300

    Trying to not raise exeception if there are parsing errors on Untied::Consumer;

--- lib/untied/consumer.rb
+++ lib/untied/consumer.rb
@@ -6,17 +6,41 @@ module Untied
     include ActiveSupport::Callbacks
     # Middleware?
     define_callbacks :process, :only => :after
+    attr_reader :observers
+
+    def initialize
+      @observers = \
+        self.class.observers.collect { |o| o.to_s.camelize.constantize.instance }
+    end
 
     def process(headers, message)
-      message = JSON.parse(message, :symbolize_keys => true)
-      payload = message[:payload]
-      service = message[:origin]
-      callback = message[:name]
-      klass = message.keys.first
+      message = begin
+                  JSON.parse(message, :symbolize_keys => true)
+                rescue JSON::ParserError
+                  {}
+                end
+
+      message = message.fetch(:event, {})
+      payload = message.fetch(:payload, {})
+      service = message[:origin].try(:to_sym)
+      event_name = message[:name].try(:to_sym)
+      klass = payload.keys.first
 
       Untied.config.logger.info \
-        "Untied::Consumer: processing message for event #{callback}"
-      run_callbacks :process do |callback, klass, service, payload|
+        "Untied::Consumer: processing message for event #{event_name}"
+
+      observers.each do |observer|
+        observer.notify(event_name, klass, service, payload)
+      end
+    end
+
+    class << self
+      def observers=(*obs)
+        @observers = obs.flatten
+      end
+
+      def observers
+        @observers ||= []
       end
     end
 
