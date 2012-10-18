17b8b65482255ca48810a24b8ba1fec86cc0d8e6
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2012-10-18 17:07:34 -0300

    Restoring filed deleted by accident

---
+++ lib/untied/railtie.rb
@@ -0,0 +1,15 @@
+# -*- encoding : utf-8 -*-
+module Untied
+  class Railtie < Rails::Railtie
+    config.after_initialize do
+      #FIXME don't know why should I do this.
+      ActiveRecord::Base.observers << Untied::PublisherObserver
+      config.active_record.observers << Untied::PublisherObserver
+      Untied::PublisherObserver.instance
+    end
+
+    rake_tasks do
+      load "untied/tasks/untied.tasks"
+    end
+  end
+end
