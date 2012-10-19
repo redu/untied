1a9ea04028c07594fb518599780836b47ef3c922
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2012-10-19 12:13:10 -0300

    Adding an empty list of observers at railstie

--- lib/untied/railtie.rb
+++ lib/untied/railtie.rb
@@ -3,6 +3,8 @@ module Untied
   class Railtie < Rails::Railtie
     config.after_initialize do
       #FIXME don't know why should I do this.
+      ActiveRecord::Base.observers ||= []
+      config.active_record.observers ||= []
       ActiveRecord::Base.observers << Untied::PublisherObserver
       config.active_record.observers << Untied::PublisherObserver
       Untied::PublisherObserver.instance
